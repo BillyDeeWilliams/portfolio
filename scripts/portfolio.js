caa8c05c408df6a93b32764145f015ac3aee8b4a
William Madden <madden.william.d@gmail.com>
2016-12-18 20:31:24 -0800

    reapplying the intake a set of objects and render to page for projects in the portfolio, as modeled in the lab with articles. thus far it is not behaving as explected. the template class is not being removed durring tohtml method

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -14,18 +14,18 @@ function Project (opts) {
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
+  var $newProject = $('div.portfolio').clone();
   $newProject.attr('data-category', this.category);
 
   $newProject.find('h1').html(this.title);
   $newProject.find('img').html(this.screenshot);
   $newProject.find('.colaborators').html(this.colaborators);
   $newProject.find('a').attr('href', this.url);
-  $newProject.find('time[pubdate]').attr('title', this.deployedOn);
-  $newProject.find('var').html('about ' + parseInt((new Date() - new Date(this.deployedOn)) / 60 / 60 / 24 / 1000 ) + ' days');
-  $newProject.find('section.description').html(this.description);
+  $newProject.find('time[pubdate]').attr('pubdate', this.deployedOn).html(this.deployedOn);
+  $newProject.find('p.live').html('about ' + parseInt((new Date() - new Date(this.deployedOn)) / 60 / 60 / 24 / 1000 ) + '<br> days');
+  $newProject.find('div.description').html(this.description);
 //remove the class of template
-  $newProject.removeAttr('class');
+  $newProject.removeClass('template');
 
   return $newProject;
 };
