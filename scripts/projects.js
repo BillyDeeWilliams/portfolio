77109c49b37c2f27c56cf69a5f1665b5bac1eed9
William Madden <madden.william.d@gmail.com>
2016-12-20 09:21:18 -0800

    template in script tag (handlebar template) is co,pleted. tba functionality and handlebar templarte render/import data is NOT working

--- scripts/projects.js
+++ scripts/projects.js
@@ -5,10 +5,11 @@ var ourLocalData = [
     category:    'website',
     screenshot: 'url.img',
     colaborators: '',
+    teamMember: 'Project Lead',
     url:   'https://billydeewilliams.github.io/about_me/',
     sourceUrl: 'http://www.github.com',
     deployedOn: '2015-11-05',
-    description: '',
+    description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
   },
   {
     title:       'Harlock',
@@ -18,6 +19,6 @@ var ourLocalData = [
     url:   'https://billydeewilliams.github.io/about_me/',
     sourceUrl: 'http://www.github.com',
     deployedOn: '2015-11-05',
-    description: '',
+    description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
   }
 ];
